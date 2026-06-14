03a984f8abfeffcdb0ef433ba55bd83962f812ba
Arfan Salman Ramadahn <arfansalman9@gmail.com>
2026-06-14 14:21:29 +0700

    rubah nama di footer

--- Front-End-PantauAlam/src/components/Footer.jsx
+++ Front-End-PantauAlam/src/components/Footer.jsx
@@ -2,10 +2,10 @@ function Footer() {
 
     return (
         <>
-            <div className="     p-4 bg-blue-400 text-white p-4 rounded-lg   mt-5 left-20">
-                <h5 className=" font-bold">
-                    Summber data: <a href="https://data.bmkg.go.id/" target="_blank" rel="noopener noreferrer" className="text-blue-200 hover:underline">BMKG</a>
-                </h5>
+            <div className="     p-4 bg-blue-400 text-white p-4 rounded-lg   mt-10 left-20">
+                <p className=" font-bold">
+                    Summber data: <a href="https://data.bmkg.go.id/" target="_blank" rel="noopener noreferrer" className="text-blue-200 hover:underline">https://data.bmkg.go.id/</a>
+                </p>
                 
             </div>
         </>
