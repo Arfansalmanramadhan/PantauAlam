fb714de78b3dc1ee1643b77f0376f8cb119e1687
Arfan Salman Ramadahn <arfansalman9@gmail.com>
2026-06-15 13:44:47 +0700

    membuat tabel di gempa bumi M5+

--- Front-End-PantauAlam/src/page/gempa/GempaTerkini.jsx
+++ Front-End-PantauAlam/src/page/gempa/GempaTerkini.jsx
@@ -72,10 +72,72 @@ function GempaTerkini() {
                         }}
                         className="rounded-xl overflow-hidden shadow-md mb-5"
                     />
+                    <div className="overflow-x-auto bg-white rounded-xl shadow-md">
+                        <table className="min-w-full">
+                            <thead className="bg-blue-300">
+                                <tr>
+                                    <th className="px-4 py-3 text-center">No</th>
+                                    <th className="px-4 py-3 text-center">Waktu</th>
+                                    <th className="px-4 py-3 text-center">Magnitude</th>
+                                    <th className="px-4 py-3 text-center">Kedalaman</th>
+                                    <th className="px-4 py-3 text-center">Koordinat</th>
+                                    <th className="px-4 py-3 text-center">Wilayah</th>
+                                    <th className="px-4 py-3 text-center">Detail</th>
+                                </tr>
+                            </thead>
 
+                            <tbody>
+                                {gempaTerkini?.map((gempa, index) => (
+                                    <tr
+                                        key={index}
+                                        className="border-b hover:bg-gray-50 transition"
+                                    >
+                                        <td className="px-4 py-3">
+                                            {index + 1}
+                                        </td>
+
+                                        <td className="px-4 py-3">
+                                            {gempa.tanggal}, {gempa.jam}
+                                        </td>
+
+
+
+                                        <td className="px-4 py-3 text-center">
+                                            <span
+                                                className={`px-2 py-1 rounded-md font-semibold ${parseFloat(gempa.magnitudo) >= 7
+                                                    ? "bg-red-100 text-red-700"
+                                                    : parseFloat(gempa.magnitudo) >= 5
+                                                        ? "bg-orange-100 text-orange-700"
+                                                        : "bg-green-100 text-green-700"
+                                                    }`}
+                                            >
+                                                {gempa.magnitudo}
+                                            </span>
+                                        </td>
+
+                                        <td className="px-4 py-3 text-center">
+                                            {gempa.Kedalaman}
+                                        </td>
+
+                                        <td className="px-4 py-3 text-center">
+                                            {gempa.Lintang}-{gempa.Bujur}
+                                        </td>
+                                        <td className="px-4 py-3 text-center">
+                                            {gempa.wilayah}
+                                        </td>
+                                        <td className="px-4 py-3">
+                                            <a href="/" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+                                                Selengkapnya
+                                            </a>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                     <Footer />
-                </Main>
-            </div>
+                </Main >
+            </div >
         </>
     )
 }
