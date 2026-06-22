c81219a65adfb49329634273a3a5b822ec341235
Arfan Salman Ramadahn <arfansalman9@gmail.com>
2026-06-22 20:03:17 +0700

    buat responsive dan subjudul di halaman gempaDirasakan dan gempaTerkini

--- Front-End-PantauAlam/src/page/gempa/GempaDirasakan.jsx
+++ Front-End-PantauAlam/src/page/gempa/GempaDirasakan.jsx
@@ -66,80 +66,83 @@ function GempaDirasakan() {
                 <Sidebar open={open} setOpen={setOpen} />
                 <Main open={open}>
                     <Header label="Gempa bumi  Dirasakan" />
-                    <div
-                        id="map"
-                        style={{
-                            height: "400px",
-                            width: "100%"
-                        }}
-                        className="rounded-xl overflow-hidden shadow-md mb-5"
-                    />
-                    <div className="overflow-x-auto bg-white rounded-xl shadow-md">
-                        <table className="min-w-full">
-                            <thead className="bg-blue-300">
-                                <tr>
-                                    <th className="px-4 py-3 text-center">No</th>
-                                    <th className="px-4 py-3 text-center">Waktu</th>
-                                    <th className="px-4 py-3 text-center">Magnitude</th>
-                                    <th className="px-4 py-3 text-center">Kedalaman</th>
-                                    <th className="px-4 py-3 text-center">Koordinat</th>
-                                    <th className="px-4 py-3 text-center">Wilayah</th>
-                                    <th className="px-4 py-3 text-center">Dirasakan</th>
-                                    <th className="px-4 py-3 text-center">Detail</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                {gempaDirasakan?.map((gempa, index) => (
-                                    <tr
-                                        key={index}
-                                        className="border-b hover:bg-gray-50 transition"
-                                    >
-                                        <td className="px-4 py-3">
-                                            {index + 1}
-                                        </td>
-
-                                        <td className="px-4 py-3">
-                                            {gempa.tanggal}, {gempa.jam}
-                                        </td>
-
-
-
-                                        <td className="px-4 py-3 text-center">
-                                            <span
-                                                className={`px-2 py-1 rounded-md font-semibold ${parseFloat(gempa.magnitudo) >= 7
-                                                    ? "bg-red-100 text-red-700"
-                                                    : parseFloat(gempa.magnitudo) >= 5
-                                                        ? "bg-orange-100 text-orange-700"
-                                                        : "bg-green-100 text-green-700"
-                                                    }`}
-                                            >
-                                                {gempa.magnitudo}
-                                            </span>
-                                        </td>
-
-                                        <td className="px-4 py-3 text-center">
-                                            {gempa.Kedalaman}
-                                        </td>
-
-                                        <td className="px-4 py-3 text-center">
-                                            {gempa.Lintang}-{gempa.Bujur}
-                                        </td>
-                                        <td className="px-4 py-3 text-center">
-                                            {gempa.wilayah}
-                                        </td>
-                                        <td className="px-4 py-3 text-center">
-                                            {gempa.Dirasakan || "Tidak ada laporan dirasakan"}
-                                        </td>
-                                        <td className="px-4 py-3">
-                                            <Link to={`/gempabumi-dirasakan/detail/${gempa.Coordinates}`} className="text-blue-600 hover:underline">
-                                                Selengkapnya
-                                            </Link>
-                                        </td>
+                    <div className="grid grid-cols-1 lg:grid-cols-1 gap-5  ">
+                        <div
+                            id="map"
+                            style={{
+                                height: "400px",
+                                width: "100%"
+                            }}
+                            className="rounded-xl overflow-hidden shadow-md mb-5 z-0"
+                        />
+                        <div className="overflow-x-auto bg-white rounded-xl shadow-md">
+                            <h2 className='text-3xl font-bold text-gray-800 mb-6 border-b pb-2'>15 Gempa Dirasakan</h2>
+                            <table className="min-w-full">
+                                <thead className="bg-blue-300">
+                                    <tr>
+                                        <th className="px-4 py-3 text-center">No</th>
+                                        <th className="px-4 py-3 text-center">Waktu</th>
+                                        <th className="px-4 py-3 text-center">Magnitude</th>
+                                        <th className="px-4 py-3 text-center">Kedalaman</th>
+                                        <th className="px-4 py-3 text-center">Koordinat</th>
+                                        <th className="px-4 py-3 text-center">Wilayah</th>
+                                        <th className="px-4 py-3 text-center">Dirasakan</th>
+                                        <th className="px-4 py-3 text-center">Detail</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                </thead>
+
+                                <tbody>
+                                    {gempaDirasakan?.map((gempa, index) => (
+                                        <tr
+                                            key={index}
+                                            className="border-b hover:bg-gray-50 transition"
+                                        >
+                                            <td className="px-4 py-3">
+                                                {index + 1}
+                                            </td>
+
+                                            <td className="px-4 py-3">
+                                                {gempa.tanggal}, {gempa.jam}
+                                            </td>
+
+
+
+                                            <td className="px-4 py-3 text-center">
+                                                <span
+                                                    className={`px-2 py-1 rounded-md font-semibold ${parseFloat(gempa.magnitudo) >= 7
+                                                        ? "bg-red-100 text-red-700"
+                                                        : parseFloat(gempa.magnitudo) >= 5
+                                                            ? "bg-orange-100 text-orange-700"
+                                                            : "bg-green-100 text-green-700"
+                                                        }`}
+                                                >
+                                                    {gempa.magnitudo}
+                                                </span>
+                                            </td>
+
+                                            <td className="px-4 py-3 text-center">
+                                                {gempa.Kedalaman}
+                                            </td>
+
+                                            <td className="px-4 py-3 text-center">
+                                                {gempa.Lintang}-{gempa.Bujur}
+                                            </td>
+                                            <td className="px-4 py-3 text-center">
+                                                {gempa.wilayah}
+                                            </td>
+                                            <td className="px-4 py-3 text-center">
+                                                {gempa.Dirasakan || "Tidak ada laporan dirasakan"}
+                                            </td>
+                                            <td className="px-4 py-3">
+                                                <Link to={`/gempabumi-dirasakan/detail/${gempa.Coordinates}`} className="text-blue-600 hover:underline">
+                                                    Selengkapnya
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                     <Footer />
                 </Main >
